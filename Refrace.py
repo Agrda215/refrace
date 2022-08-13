4fc64903a4281ee859d088c34f2ff36543a535ba
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-13 16:00:30 -0300

    ok

---
+++ Refrace.py
@@ -0,0 +1,10 @@
+score = 0
+highscore = 0
+top = 0
+bottom = 0
+
+print("Hello 4000 Scores! of Where Of Refance.")
+print("Whoah Here Based in here.")
+print("Top:".join(top))
+print("Bottom:".join(bottom))
+print("Score:".join(score))
