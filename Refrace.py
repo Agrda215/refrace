e9f751bbd706c47cda96c36d139bacfd96f5ac56
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-13 16:40:38 -0300

    printing

--- Refrace.py
+++ Refrace.py
@@ -1,10 +1,28 @@
+from _typeshed import Incomplete
+
+
 score = 0
 highscore = 0
 top = 0
 bottom = 0
 
-print("Hello 4000 Scores! of Where Of Refance.")
+print("Hello 4000 Scores! of Where Of Reface.")
 print("Whoah Here Based in here.")
 print("Top:",top)
 print("Bottom:", bottom)
 print("Score:", score)
+print("Inputed:")
+inputed = input()
+
+def printing():
+    score += 1
+    print("Top:",top)
+    print("Bottom:", bottom)
+    print("Score:", score)
+    print("Inputed:")
+    inputed = input()
+
+if top >= inputed:
+    printing()
+else:
+    print("Whoah than ", top)
